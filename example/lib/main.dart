6beb1ab002ed96eff55ddcafdb948a1a88757c42
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-05-29 19:02:00 -0300

    feat: add example to use printer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String _retorno = 'Unknown';
   final _pluginFlutterElginPlugin = PluginFlutterElgin();
 
   @override
@@ -31,8 +31,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _pluginFlutterElginPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _pluginFlutterElginPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -43,7 +43,105 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _retorno = platformVersion;
+    });
+  }
+
+  Future<void> abrirConexao() async {
+    String retorno;
+    // try {
+    retorno = await _pluginFlutterElginPlugin.printer
+        .abreConexaoImpressora(5, '', '', 0);
+    // } catch (_) {
+    //   retorno = -2222;
+    // }
+
+    if (!mounted) return;
+
+    setState(() {
+      _retorno = retorno.toString();
+    });
+  }
+
+  Future<void> fechaConexao() async {
+    String retorno;
+    try {
+      retorno = await _pluginFlutterElginPlugin.printer.fechaConexao();
+    } catch (e) {
+      retorno = '-2222';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _retorno = retorno;
+    });
+  }
+
+  Future<void> avancaPapel() async {
+    String retorno;
+    try {
+      retorno = await _pluginFlutterElginPlugin.printer.avancaPapel(5);
+    } catch (e) {
+      retorno = '-2222';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _retorno = retorno;
+    });
+  }
+
+  Future<void> corte() async {
+    String retorno;
+    try {
+      retorno = await _pluginFlutterElginPlugin.printer.corte(2);
+    } catch (e) {
+      retorno = '-2222';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _retorno = retorno;
+    });
+  }
+
+  Future<void> imprimeTexto() async {
+    String retorno;
+    try {
+      retorno = await _pluginFlutterElginPlugin.printer
+          .impressaoTexto('Teste Flutter Plugin', 0, 0, 2);
+    } catch (e) {
+      retorno = '-2222';
+      rethrow;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _retorno = retorno;
+    });
+  }
+
+  Future<void> impressaoXMLSAT() async {
+    String retorno;
+    try {
+      String xmlSat =
+          '<?xml version="1.0"?><CFe><infCFe versaoDadosEnt="0.07"><ide><CNPJ>08427847000169</CNPJ><signAC>SGR-SAT SISTEMA DE GESTAO E RETAGUARDA DO SAT</signAC><numeroCaixa>001</numeroCaixa></ide><emit><CNPJ>61099008000141</CNPJ><IE>111111111111</IE><IM>12345</IM><cRegTribISSQN>3</cRegTribISSQN><indRatISSQN>N</indRatISSQN></emit><dest/><det nItem="1"><prod><cProd>116</cProd><cEAN>9990000001163</cEAN><xProd>Cascao</xProd><CFOP>5405</CFOP><uCom>UN</uCom><qCom>1.0000</qCom><vUnCom>4.00</vUnCom><indRegra>A</indRegra></prod><imposto><ICMS><ICMSSN102><Orig>0</Orig><CSOSN>500</CSOSN></ICMSSN102></ICMS><PIS><PISSN><CST>49</CST></PISSN></PIS><COFINS><COFINSSN><CST>49</CST></COFINSSN></COFINS></imposto></det><total/><pgto><MP><cMP>01</cMP><vMP>4.00</vMP></MP></pgto></infCFe></CFe>';
+      xmlSat = xmlSat.replaceAll('"', "'");
+      retorno =
+          await _pluginFlutterElginPlugin.printer.imprimeXMLSAT(xmlSat, 0);
+    } catch (e) {
+      retorno = '-2222';
+      rethrow;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _retorno = retorno;
     });
   }
 
@@ -55,8 +153,54 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+            child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 50, 0, 50),
+              child: Text(
+                'Retorno: $_retorno',
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 50),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: abrirConexao,
+                      child: const Text('Abre Conexão')),
+                  ElevatedButton(
+                      onPressed: fechaConexao,
+                      child: const Text('Fecha Conexão')),
+                  ElevatedButton(
+                      onPressed: avancaPapel, child: const Text('Avança Papel'))
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 50),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: corte, child: const Text('Corte Papel')),
+                  ElevatedButton(
+                      onPressed: imprimeTexto,
+                      child: const Text('Imprime Texto')),
+                  ElevatedButton(
+                      onPressed: impressaoXMLSAT,
+                      child: const Text('Imprime Cupom SAT'))
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
