95c28f38c5f72e1cffeea78120b2efc1eabd3a82
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-06-02 20:06:47 -0300

    feat: add generateParmsString and makeTransaction
    Abstract from methods some repetitive concepts and implement them

--- lib/src/printer/printer_method_channel.dart
+++ lib/src/printer/printer_method_channel.dart
@@ -11,77 +11,82 @@ class PrinterMethodChannel extends PrinterPlatform {
       : androidIntent = AndroidIntent(Device.printerActivityFilterPath);
 
   @override
+  String generateParamsString(Map<String, dynamic> params) {
+    List<String> paramList = [];
+
+    params.forEach((key, value) {
+      String paramValue;
+
+      if (value is String) {
+        paramValue = '"$value"';
+      } else {
+        paramValue = value.toString();
+      }
+
+      paramList.add('"$key": $paramValue');
+    });
+
+    return '{${paramList.join(', ')}}';
+  }
+
+  @override
+  Future<dynamic> makeTransaction(
+      String functionName, Map<String, dynamic> params) async {
+    String paramsString = generateParamsString(params);
+    Map<String, dynamic> arguments =
+        androidIntent.buildArguments(functionName, paramsString);
+    final ret = await androidIntent.sendIntent(arguments);
+    if (ret == null) return throw Error();
+    final idhReturn = androidIntent.extractListResult(ret);
+
+    debugPrint(functionName);
+    debugPrint(paramsString);
+    debugPrint(arguments.toString());
+    debugPrint('Return idh: $idhReturn');
+
+    return idhReturn;
+  }
+
+  @override
   Future<int> abreConexaoImpressora(
       int tipo, String modelo, String conexao, int parametro) async {
     String functionName = 'AbreConexaoImpressora';
 
-    String params =
-        '{"tipo": $tipo, "modelo": "$modelo", "conexao": "$conexao", "parametro": $parametro}';
+    Map<String, dynamic> params = {
+      'tipo': tipo,
+      'modelo': modelo,
+      'conexao': conexao,
+      'parametro': parametro
+    };
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn = await makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> fechaConexaoImpressora() async {
     String functionName = 'FechaConexaoImpressora';
-    String params = '{}';
+    Map<String, dynamic> params = {};
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn = await makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> avancaPapel(int linhas) async {
     String functionName = 'AvancaPapel';
-    String params = '{"linhas": $linhas }';
+    Map<String, dynamic> params = {'linhas': linhas};
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn = await makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> corte(int linhas) async {
     String functionName = 'Corte';
-    String params = '{"avanco": $linhas }';
+    Map<String, dynamic> params = {'avanco': linhas};
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn = await makeTransaction(functionName, params);
     return idhReturn;
   }
 
@@ -90,31 +95,25 @@ class PrinterMethodChannel extends PrinterPlatform {
       String texto, int alinhamento, int fonte, int tamanhoFonte) async {
     debugPrint('im here');
     String functionName = 'ImpressaoTexto';
-    String params = '{'
-        '"dados": "$texto",'
-        '"posicao": $alinhamento,'
-        '"stilo": $fonte,'
-        '"tamanho": $tamanhoFonte'
-        '}';
+    Map<String, dynamic> params = {
+      'dados': texto,
+      'posicao': alinhamento,
+      'stilo': fonte,
+      'tamanho': tamanhoFonte
+    };
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn = await makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> imprimeXMLSAT(String data, int param) async {
-    debugPrint('im here');
     String functionName = 'ImprimeXMLSAT';
-    String params = '{"dados": "$data", "param": $param}';
+    Map<String, dynamic> params = {'dados': data, 'param': param};
+
+    final int idhReturn = await makeTransaction(functionName, params);
+    return idhReturn;
+  }
 
     Map<String, dynamic> arguments =
         androidIntent.buildArguments(functionName, params);
