18460ba7cab824c1c8c769f2c05470b1d2408dd1
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-06-03 13:54:46 -0300

    feat(pr/printer (#7)): finish adding printer methods
    * feat: add unescape function
    
    function that cleans a string from the non necessary escape characters
    
    * feat: util to unescape jsons members on return
    
    * feat: unescaped members of class
    
    * feat: add generateParmsString and makeTransaction
    
    Abstract from methods some repetitive concepts and implement them
    
    * feat: add remaining printer functions
    
    * feat: update example to print cupom after transact
    
    * refactor: abstract method to be used elsewhere
    
    * refactor: concentrate defines in separate files

--- lib/src/printer/printer_method_channel.dart
+++ lib/src/printer/printer_method_channel.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:plugin_flutter_elgin/src/printer/defines/defines.dart';
 import 'package:plugin_flutter_elgin/src/utils/device.dart';
 import 'package:plugin_flutter_elgin/src/utils/android_intent_elgin.dart';
 
@@ -13,119 +13,282 @@ class PrinterMethodChannel extends PrinterPlatform {
   @override
   Future<int> abreConexaoImpressora(
       int tipo, String modelo, String conexao, int parametro) async {
-    String functionName = 'AbreConexaoImpressora';
+    String functionName = DefineFunctions.abreConexaoImpressora;
 
-    String params =
-        '{"tipo": $tipo, "modelo": "$modelo", "conexao": "$conexao", "parametro": $parametro}';
+    Map<String, dynamic> params = {
+      DefineParams.tipo: tipo,
+      DefineParams.modelo: modelo,
+      DefineParams.conexao: conexao,
+      DefineParams.parametro: parametro
+    };
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> fechaConexaoImpressora() async {
-    String functionName = 'FechaConexaoImpressora';
-    String params = '{}';
+    String functionName = DefineFunctions.fechaConexaoImpressora;
+    Map<String, dynamic> params = {};
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> avancaPapel(int linhas) async {
-    String functionName = 'AvancaPapel';
-    String params = '{"linhas": $linhas }';
+    String functionName = DefineFunctions.avancaPapel;
+    Map<String, dynamic> params = {DefineParams.linhas: linhas};
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> corte(int linhas) async {
-    String functionName = 'Corte';
-    String params = '{"avanco": $linhas }';
+    String functionName = DefineFunctions.corte;
+    Map<String, dynamic> params = {DefineParams.avanco: linhas};
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> impressaoTexto(
       String texto, int alinhamento, int fonte, int tamanhoFonte) async {
-    debugPrint('im here');
-    String functionName = 'ImpressaoTexto';
-    String params = '{'
-        '"dados": "$texto",'
-        '"posicao": $alinhamento,'
-        '"stilo": $fonte,'
-        '"tamanho": $tamanhoFonte'
-        '}';
+    String functionName = DefineFunctions.impressaoTexto;
+    Map<String, dynamic> params = {
+      DefineParams.dados: texto,
+      DefineParams.posicao: alinhamento,
+      DefineParams.stilo: fonte,
+      DefineParams.tamanho: tamanhoFonte
+    };
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
-
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
     return idhReturn;
   }
 
   @override
   Future<int> imprimeXMLSAT(String data, int param) async {
-    debugPrint('im here');
-    String functionName = 'ImprimeXMLSAT';
-    String params = '{"dados": "$data", "param": $param}';
+    String functionName = DefineFunctions.imprimeXMLSAT;
+    Map<String, dynamic> params = {
+      DefineParams.dados: data,
+      DefineParams.param: param
+    };
 
-    Map<String, dynamic> arguments =
-        androidIntent.buildArguments(functionName, params);
-    final ret = await androidIntent.sendIntent(arguments);
-    if (ret == null) return -1111;
-    final int idhReturn = androidIntent.extractListResult(ret);
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
 
-    debugPrint(functionName);
-    debugPrint(params);
-    debugPrint(arguments.toString());
-    debugPrint('Return idh: $idhReturn');
+  @override
+  Future<int> abreGaveta(int pino, int ti, int tf) async {
+    String functionName = DefineFunctions.abreGaveta;
+    Map<String, dynamic> params = {
+      DefineParams.pino: pino,
+      DefineParams.ti: ti,
+      DefineParams.tf: tf
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> abreGavetaElgin() async {
+    String functionName = DefineFunctions.abreGavetaElgin;
+    Map<String, dynamic> params = {};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> corteTotal(int avanco) async {
+    String functionName = DefineFunctions.corteTotal;
+    Map<String, dynamic> params = {DefineParams.avanco: avanco};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> definePosicao(int posicao) async {
+    String functionName = DefineFunctions.definePosicao;
+    Map<String, dynamic> params = {DefineParams.posicao: posicao};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> espacamentoEntreLinhas() async {
+    String functionName = DefineFunctions.espacamentoEntreLinhas;
+    Map<String, dynamic> params = {};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> impressaoCodigoBarras(
+      int tipo, String dados, int altura, int largura, int hri) async {
+    String functionName = DefineFunctions.impressaoCodigoBarras;
+    Map<String, dynamic> params = {
+      DefineParams.tipo: tipo,
+      DefineParams.dados: dados,
+      DefineParams.altura: altura,
+      DefineParams.largura: largura,
+      DefineParams.hri: hri
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> impressaoPDF417(int numCols, int numRows, int width, int height,
+      int errCorLvl, int options, String dados) async {
+    String functionName = DefineFunctions.impressaoPDF417;
+    Map<String, dynamic> params = {
+      DefineParams.numCols: numCols,
+      DefineParams.numRows: numRows,
+      DefineParams.width: width,
+      DefineParams.height: height,
+      DefineParams.errCorLvl: errCorLvl,
+      DefineParams.options: options,
+      DefineParams.dados: dados
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> impressaoQRCode(
+      String dados, int tamanho, int nivelCorrecao) async {
+    String functionName = DefineFunctions.impressaoQRCode;
+    Map<String, dynamic> params = {
+      DefineParams.dados: dados,
+      DefineParams.tamanho: tamanho,
+      DefineParams.nivelCorrecao: nivelCorrecao
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> imprimeImagem(String path) async {
+    String functionName = DefineFunctions.imprimeImagem;
+    Map<String, dynamic> params = {DefineParams.path: path};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> imprimeImageMemoria(String key, int scala) async {
+    String functionName = DefineFunctions.imprimeImagemMemoria;
+    Map<String, dynamic> params = {
+      DefineParams.key: key,
+      DefineParams.scala: scala
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> imprimeXMLCancelamentoNFCe(String dados, int param) async {
+    String functionName = DefineFunctions.imprimeXMLCancelamentoNFCe;
+    Map<String, dynamic> params = {
+      DefineParams.dados: dados,
+      DefineParams.param: param
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> imprimeXMLCancelamentoSAT(
+      String dados, String assQRCode, int param) async {
+    String functionName = DefineFunctions.imprimeXMLCancelamentoSAT;
+    Map<String, dynamic> params = {
+      DefineParams.dados: dados,
+      DefineParams.assQRCode: assQRCode,
+      DefineParams.param: param
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> imprimeXMLNFCe(
+      String dados, int indexcsc, String csc, int param) async {
+    String functionName = DefineFunctions.imprimeXMLNFCe;
+    Map<String, dynamic> params = {
+      DefineParams.dados: dados,
+      DefineParams.indexcsc: indexcsc,
+      DefineParams.csc: csc,
+      DefineParams.param: param
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> inicializaImpressora() async {
+    String functionName = DefineFunctions.inicializaImpressora;
+    Map<String, dynamic> params = {};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> sinalSonoro(int qtd, int tempoInicio, int tempoFim) async {
+    String functionName = DefineFunctions.sinalSonoro;
+    Map<String, dynamic> params = {
+      DefineParams.qtd: qtd,
+      DefineParams.tempoInicio: tempoInicio,
+      DefineParams.tempoFim: tempoFim
+    };
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
+    return idhReturn;
+  }
+
+  @override
+  Future<int> statusImpressora(int param) async {
+    String functionName = DefineFunctions.statusImpressora;
+    Map<String, dynamic> params = {DefineParams.param: param};
+
+    final int idhReturn =
+        await androidIntent.makeTransaction(functionName, params);
     return idhReturn;
   }
 }
--- lib/src/utils/android_intent_elgin.dart
+++ lib/src/utils/android_intent_elgin.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class AndroidIntent {
@@ -36,5 +37,136 @@ class AndroidIntent {
   dynamic extractListResult(String response) {
     final List<dynamic> json = jsonDecode(response);
     return json[0]['resultado'];
+  }
+
+  static String? unescapeIdhReturn(Map<String, dynamic> json, String key) {
+    String? value = json[key];
+    if (value != null) {
+      value = unescape(value);
+    }
+    return value;
+  }
+
+  String generateParamsString(Map<String, dynamic> params) {
+    List<String> paramList = [];
+
+    params.forEach((key, value) {
+      String paramValue;
+
+      if (value is String) {
+        paramValue = '"$value"';
+      } else {
+        paramValue = value.toString();
+      }
+
+      paramList.add('"$key": $paramValue');
+    });
+
+    return '{${paramList.join(', ')}}';
+  }
+
+  Future<dynamic> makeTransaction(
+      String functionName, Map<String, dynamic> params) async {
+    String paramsString = generateParamsString(params);
+    Map<String, dynamic> arguments = buildArguments(functionName, paramsString);
+    final ret = await sendIntent(arguments);
+    if (ret == null) return throw Error();
+    final idhReturn = extractListResult(ret);
+
+    debugPrint(functionName);
+    debugPrint(paramsString);
+    debugPrint(arguments.toString());
+    debugPrint('Return idh: $idhReturn');
+
+    return idhReturn;
+  }
+
+  static String unescape(String input) {
+    final sb = StringBuffer();
+
+    while (input.isNotEmpty) {
+      int index = input.indexOf("\\");
+      // No escaped characters
+      if (index == -1) {
+        sb.write(input);
+        break;
+      }
+      sb.write(input.substring(0, index));
+      // Forward slash at the end of text. Ignore.
+      if (index == input.length - 1) {
+        break;
+      }
+      String select = String.fromCharCode(input.codeUnitAt(index + 1));
+      input = input.substring(index + 2);
+      switch (select) {
+        case '\\':
+          sb.write('\\');
+          break;
+        case 't':
+          sb.write('\t');
+          break;
+        case 'r':
+          sb.write('\r');
+          break;
+        case 'n':
+          sb.write('\n');
+          break;
+        case 'f':
+          sb.write('\f');
+          break;
+        case 'b':
+          sb.write('\b');
+          break;
+        case 'v':
+          sb.write('\v');
+          break;
+        case 'u':
+          if (input.length < 4) {
+            input = '';
+            break;
+          }
+          if (input[0] != '{') {
+            String digit = input.substring(0, 4);
+            int? intDigit = int.tryParse(digit, radix: 16);
+            if (intDigit == null || intDigit < 0) {
+              break;
+            }
+            input = input.substring(4);
+            sb.writeCharCode(intDigit);
+          } else {
+            final match = RegExp(r"{([a-zA-Z0-9]+)}").matchAsPrefix(input);
+            if (match == null) {
+              break;
+            } else {
+              input = input.substring(match.end);
+              String digit = match[1]!;
+              int? intDigit = int.tryParse(digit, radix: 16);
+              if (intDigit == null || intDigit < 0) {
+                break;
+              }
+              sb.writeCharCode(intDigit);
+            }
+          }
+          break;
+        case 'x':
+          if (input.length < 2) {
+            input = '';
+            break;
+          }
+          String digit = input.substring(0, 2);
+          input = input.substring(2);
+          int? intDigit = int.tryParse(digit, radix: 16);
+          if (intDigit == null || intDigit < 0) {
+            break;
+          }
+          sb.writeCharCode(intDigit);
+          break;
+        default:
+          sb.write(select);
+          break;
+      }
+    }
+
+    return sb.toString();
   }
 }
