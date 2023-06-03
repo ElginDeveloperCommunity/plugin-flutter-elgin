0eab6a8aff0381d68c02ba93b5b76ddeb2fd1583
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-06-03 16:05:39 -0300

    feat: add sat methods

---
+++ lib/src/sat/defines/define_functions.dart
@@ -0,0 +1,9 @@
+abstract class DefineFunctions {
+  static const String associarAssinatura = 'associarAssinatura';
+  static const String ativarSAT = 'ativarSAT';
+  static const String cancelarUltimaVenda = 'cancelarUltimaVenda';
+  static const String consultarSAT = 'consultarSAT';
+  static const String consultarStatusOperacional = 'consultarStatusOperacional';
+  static const String enviarDadosVenda = 'enviarDadosVenda';
+  static const String extrairLogs = 'extrairLogs';
+}
--- lib/src/utils/device.dart
+++ lib/src/utils/device.dart
@@ -2,4 +2,5 @@ abstract class Device {
   static const String printerActivityFilterPath =
       'com.elgin.e1.digitalhub.TERMICA';
   static const String tefActivityFilterPath = 'com.elgin.e1.digitalhub.TEF';
+  static const String satActivityFilterPath = 'com.elgin.e1.digitalhub.SAT';
 }
